1104f05c3d273913a6ce856768f6ebf6fbe4c808
Andrew Leitsius <leits@quintagroup.com>
2017-08-15 15:52:29 +0300

    Update validation

--- openregistry/api/validation.py
+++ openregistry/api/validation.py
@@ -1,6 +1,5 @@
 from schematics.exceptions import ModelValidationError, ModelConversionError
-from openregistry.api.utils import apply_data_patch, update_logging_context
-from openregistry.api.utils import raise_operation_error
+from openregistry.api.utils import apply_data_patch, update_logging_context, error_handler, raise_operation_error
 
 
 def validate_json_data(request):
@@ -9,11 +8,11 @@ def validate_json_data(request):
     except ValueError, e:
         request.errors.add('body', 'data', e.message)
         request.errors.status = 422
-        return
+        raise error_handler(request)
     if not isinstance(json, dict) or 'data' not in json or not isinstance(json.get('data'), dict):
         request.errors.add('body', 'data', "Data not available")
         request.errors.status = 422
-        return
+        raise error_handler(request)
     request.validated['json_data'] = json['data']
     return json['data']
 
@@ -21,8 +20,6 @@ def validate_json_data(request):
 def validate_data(request, model, partial=False, data=None):
     if data is None:
         data = validate_json_data(request)
-    if data is None:
-        return
     try:
         if partial and isinstance(request.context, model):
             initial_data = request.context.serialize()
@@ -44,16 +41,16 @@ def validate_data(request, model, partial=False, data=None):
         for i in e.message:
             request.errors.add('body', i, e.message[i])
         request.errors.status = 422
-        data = None
+        raise error_handler(request)
     except ValueError, e:
         request.errors.add('body', 'data', e.message)
         request.errors.status = 422
-        data = None
+        raise error_handler(request)
     else:
         if hasattr(type(m), '_options') and role not in type(m)._options.roles:
             request.errors.add('url', 'role', 'Forbidden')
             request.errors.status = 403
-            data = None
+            raise error_handler(request)
         else:
             data = method(role)
             request.validated['data'] = data
@@ -61,7 +58,6 @@ def validate_data(request, model, partial=False, data=None):
                 m = model(data)
                 m.__parent__ = request.context
                 request.validated[model.__name__.lower()] = m
-
     return data
 
 
@@ -82,31 +78,12 @@ def validate_change_status(request, error_handler, **kwargs):
         return
 
     # get available statuses from dict
-    statuses = request.content_configurator.available_statuses[model.status]
+    statuses = request.content_configurator.available_statuses[model.status]['next_status']
     # verify right status change (auth_role and target status)
     msg = 'Can\'t update {} in current ({}) status'.format(resource_type,
                                                            model.status)
+
     if new_status not in statuses or \
             request.authenticated_role not in statuses.get(new_status, {}):
         raise_operation_error(request, error_handler, msg)
 
-
-def validate_terminated_statuses(request, error_handler, **kwargs):
-    """
-        This validator get terminated statuses from configurator and validate
-        impossibility to change terminated status
-    """
-    # Get resource_type
-    resource_type = request.validated['resource_type']
-
-    # get context
-    model = request.context
-
-    # get terminated_statuses
-    terminated_statuses = request.content_configurator.terminated_statuses
-
-    # check context status
-    if model.status in terminated_statuses:
-        msg = 'Can\'t update {} in current ({}) status'.format(resource_type,
-                                                               model.status)
-        raise_operation_error(request, error_handler, msg)
