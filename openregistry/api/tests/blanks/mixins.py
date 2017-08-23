8d4a439449f6348af828adcb886c7f17d351a34b
sasha-kantoriz <sasha-kantoriz@quintagroup.org>
2017-08-23 14:33:38 +0300

    Add ResourceTestMixin with common blanks

--- openregistry/api/tests/blanks/mixins.py
+++ openregistry/api/tests/blanks/mixins.py
@@ -3,6 +3,15 @@
 from openregistry.api.tests.base import snitch
 
 from .core_resource import empty_listing
+from .resource import (
+    listing,
+    listing_changes,
+    listing_draft,
+    resource_not_found,
+    dateModified_resource,
+    get_resource,
+    create_resource
+)
 
 
 class CoreResourceTestMixin(object):
@@ -10,3 +19,16 @@ class CoreResourceTestMixin(object):
     """
 
     test_empty_listing = snitch(empty_listing)
+
+
+class ResourceTestMixin(object):
+    """ Mixin with common tests for Asset and Lot
+    """
+
+    test_01_listing = snitch(listing)
+    test_02_listing_changes = snitch(listing_changes)
+    test_03_listing_draft = snitch(listing_draft)
+    test_04_resource_not_found = snitch(resource_not_found)
+    test_05_dateModified_resource = snitch(dateModified_resource)
+    test_06_get_resource = snitch(get_resource)
+    test_07_create_resource = snitch(create_resource)
