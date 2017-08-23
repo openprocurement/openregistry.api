61ad97eeb19be852a37eb84d5e0f288e0a6e45ac
sasha-kantoriz <sasha-kantoriz@quintagroup.org>
2017-08-23 16:57:34 +0300

    Add ResourceDocumentTestMixin

--- openregistry/api/tests/blanks/mixins.py
+++ openregistry/api/tests/blanks/mixins.py
@@ -12,6 +12,16 @@ from .resource import (
     get_resource,
     create_resource
 )
+from .document import (
+    not_found,
+    create_document_in_forbidden_resource_status,
+    put_resource_document_invalid,
+    patch_resource_document,
+    create_resource_document_error,
+    create_resource_document_json_invalid,
+    create_resource_document_json,
+    put_resource_document_json
+)
 
 
 class CoreResourceTestMixin(object):
@@ -32,3 +42,17 @@ class ResourceTestMixin(object):
     test_05_dateModified_resource = snitch(dateModified_resource)
     test_06_get_resource = snitch(get_resource)
     test_07_create_resource = snitch(create_resource)
+
+
+class ResourceDocumentTestMixin(object):
+    """ Mixin with common tests for Asset and Lot documents
+    """
+
+    test_01_not_found = snitch(not_found)
+    test_02_create_document_in_forbidden_resource_status = snitch(create_document_in_forbidden_resource_status)
+    test_03_put_resource_document_invalid = snitch(put_resource_document_invalid)
+    test_04_patch_resource_document = snitch(patch_resource_document)
+    test_05_create_resource_document_error = snitch(create_resource_document_error)
+    test_06_create_resource_document_json_invalid = snitch(create_resource_document_json_invalid)
+    test_07_create_resource_document_json = snitch(create_resource_document_json)
+    test_08_put_resource_document_json = snitch(put_resource_document_json)
