67212210a7a014c4adefb34304a8836e6fa5e5fe
sasha-kantoriz <sasha-kantoriz@quintagroup.org>
2017-09-01 12:35:53 +0300

    Add file with data for tests

---
+++ openregistry/api/tests/blanks/json_data.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+from copy import deepcopy
+
+
+test_organization = {
+    "name": u"Державне управління справами",
+    "identifier": {
+        "scheme": u"UA-EDR",
+        "id": u"00037256",
+        "uri": u"http://www.dus.gov.ua/"
+    },
+    "address": {
+        "countryName": u"Україна",
+        "postalCode": u"01220",
+        "region": u"м. Київ",
+        "locality": u"м. Київ",
+        "streetAddress": u"вул. Банкова, 11, корпус 1"
+    },
+    "contactPoint": {
+        "name": u"Державне управління справами",
+        "telephone": u"0440000000"
+    }
+}
+
+test_document_data = {
+    # 'url': self.generate_docservice_url(),
+    'title': u'укр.doc',
+    'hash': 'md5:' + '0' * 32,
+    'format': 'application/msword',
+}
+
+test_asset_data = {
+    "title": u"Земля для космодрому",
+    "assetType": "basic",
+    "assetCustodian": deepcopy(test_organization),
+    "classification": {
+        "scheme": u"CPV",
+        "id": u"37452200-3",
+        "description": u"Земельні ділянки"
+    },
+    "unit": {
+        "name": u"item",
+        "code": u"44617100-9"
+    },
+    "quantity": 5,
+    "address": {
+        "countryName": u"Україна",
+        "postalCode": "79000",
+        "region": u"м. Київ",
+        "locality": u"м. Київ",
+        "streetAddress": u"вул. Банкова 1"
+    },
+    "value": {
+        "amount": 100,
+        "currency": u"UAH"
+    },
+}
+
+test_lot_data = {
+    "title": u"Тестовий лот",
+    "description": u"Щось там тестове",
+    "lotType": "basic",
+    "lotCustodian": deepcopy(test_organization),
+    "assets": []
+}
