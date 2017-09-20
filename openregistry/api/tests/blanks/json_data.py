5194a5a027cc8b503ea7425b1804adfcc48bf269
Andrew Leitsius <leits@quintagroup.com>
2017-09-20 15:11:35 +0300

    Add items to compound test data

--- openregistry/api/tests/blanks/json_data.py
+++ openregistry/api/tests/blanks/json_data.py
@@ -29,6 +29,34 @@ test_document_data = {
     'format': 'application/msword',
 }
 
+test_item_data = {
+    "description": u"футляри до державних нагород",
+    "classification": {
+        "scheme": u"CAV",
+        "id": u"39513200-3",
+        "description": u"Cartons"
+    },
+    "additionalClassifications": [
+        {
+            "scheme": u"ДКПП",
+            "id": u"17.21.1",
+            "description": u"папір і картон гофровані, паперова й картонна тара"
+        }
+    ],
+    "unit": {
+        "name": u"item",
+        "code": u"44617100-9"
+    },
+    "quantity": 5,
+    "address": {
+        "countryName": u"Україна",
+        "postalCode": "79000",
+        "region": u"м. Київ",
+        "locality": u"м. Київ",
+        "streetAddress": u"вул. Банкова 1"
+    }
+}
+
 test_asset_basic_data = {
     "title": u"Земля для космодрому",
     "assetType": "basic",
@@ -56,8 +84,25 @@ test_asset_basic_data = {
     },
 }
 
+test_debt_data = {
+    "agreementNumber": u"42",
+    "debtorType": u"legalPerson",
+    "dateSigned": u"2017-08-16T12:30:17.615196+03:00",
+    "value": {
+        "amount": 1,
+        "currency": u"UAH"
+    },
+    "debtCurrencyValue": {
+        "amount": 100,
+        "currency": u"USD"
+    },
+}
+
 test_asset_compound_data = deepcopy(test_asset_basic_data)
 test_asset_compound_data['assetType'] = 'compound'
+
+test_asset_compound_data['items'] = [test_item_data, test_item_data]
+
 
 
 test_lot_data = {
