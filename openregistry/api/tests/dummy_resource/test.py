e5dcf09384d77eff331ce5134171ba1c1fa50a6b
Andrew Leitsius <leits@quintagroup.com>
2017-09-21 12:52:15 +0300

    Fix tests

--- openregistry/api/tests/dummy_resource/test.py
+++ openregistry/api/tests/dummy_resource/test.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import os
-import webtest
 import unittest
 from pyramid import testing
 from mock import Mock, MagicMock, patch
@@ -13,118 +12,118 @@ settings = appconfig('config:' + os.path.join(os.path.dirname(__file__), '..', '
 
 
 class BaseAPIUnitTest(unittest.TestCase):
-	""" Base TestCase class for unit tests of API functionality
-	"""
+    """ Base TestCase class for unit tests of API functionality
+    """
 
-	def setUp(self):
-		self.config = testing.setUp()
+    def setUp(self):
+        self.config = testing.setUp()
 
-	def tearDown(self):
-		testing.tearDown()
+    def tearDown(self):
+        testing.tearDown()
 
 
 class TestAPIResourceListing(BaseAPIUnitTest):
-	""" TestCase for APIResource and APIResourceListing
-	"""
+    """ TestCase for APIResource and APIResourceListing
+    """
 
-	def setUp(self):
-		super(TestAPIResourceListing, self).setUp()
-		self.config.include('cornice')
-		self.config.scan('.views')
+    def setUp(self):
+        super(TestAPIResourceListing, self).setUp()
+        self.config.include('cornice')
+        self.config.scan('.views')
 
-		self.request = testing.DummyRequest()
-		self.request.registry.db = Mock()
-		self.request.registry.server_id = Mock()
-		self.request.registry.couchdb_server = Mock()
-		self.request.registry.update_after = True
+        self.request = testing.DummyRequest()
+        self.request.registry.db = Mock()
+        self.request.registry.server_id = Mock()
+        self.request.registry.couchdb_server = Mock()
+        self.request.registry.update_after = True
 
-		self.context = Mock()
+        self.context = Mock()
 
-	def test_01_listing(self):
+    def test_01_listing(self):
 
-		view = DummyResource(self.request, self.context)
-		response = view.get()
+        view = DummyResource(self.request, self.context)
+        response = view.get()
 
-		self.assertEqual(response['data'], [])
-		self.assertNotIn('prev_page', response)
+        self.assertEqual(response['data'], [])
+        self.assertNotIn('prev_page', response)
 
-	def test_02_listing_opt_fields(self):
-		
-		self.request.params['opt_fields'] = 'status'
-		self.request.logging_context = MagicMock()
+    def test_02_listing_opt_fields(self):
 
-		view = DummyResource(self.request, self.context)
-		response = view.get()
+        self.request.params['opt_fields'] = 'status'
+        self.request.logging_context = MagicMock()
 
-		self.assertEqual(response['data'], [])
+        view = DummyResource(self.request, self.context)
+        response = view.get()
 
-	def test_03_listing_limit(self):
+        self.assertEqual(response['data'], [])
 
-		self.request.params['limit'] = '10'
+    def test_03_listing_limit(self):
 
-		view = DummyResource(self.request, self.context)
-		response = view.get()
+        self.request.params['limit'] = '10'
 
-		self.assertEqual(response['data'], [])
+        view = DummyResource(self.request, self.context)
+        response = view.get()
 
-	def test_04_listing_offset(self):
-		
-		self.request.params['offset'] = '2015-01-01T00:00:00+02:00'
+        self.assertEqual(response['data'], [])
 
-		view = DummyResource(self.request, self.context)
-		response = view.get()
+    def test_04_listing_offset(self):
 
-		self.assertEqual(response['data'], [])
+        self.request.params['offset'] = '2015-01-01T00:00:00+02:00'
 
-	def test_05_listing_descending(self):
+        view = DummyResource(self.request, self.context)
+        response = view.get()
 
-		self.request.params['descending'] = '1'
+        self.assertEqual(response['data'], [])
 
-		view = DummyResource(self.request, self.context)
-		response = view.get()
+    def test_05_listing_descending(self):
 
-		self.assertEqual(response['data'], [])
+        self.request.params['descending'] = '1'
 
-	def test_06_listing_feed(self):
-		
-		self.request.params['feed'] = 'changes'
+        view = DummyResource(self.request, self.context)
+        response = view.get()
 
-		view = DummyResource(self.request, self.context)
-		response = view.get()
+        self.assertEqual(response['data'], [])
 
-		self.assertEqual(response['data'], [])
+    def test_06_listing_feed(self):
+
+        self.request.params['feed'] = 'changes'
+
+        view = DummyResource(self.request, self.context)
+        response = view.get()
+
+        self.assertEqual(response['data'], [])
 
 
-	def test_07_listing_mode(self):
-		
-		self.request.params['mode'] = u'test'
+    def test_07_listing_mode(self):
 
-		view = DummyResource(self.request, self.context)
-		response = view.get()
+        self.request.params['mode'] = u'test'
 
-		self.assertEqual(response['data'], [])
+        view = DummyResource(self.request, self.context)
+        response = view.get()
 
-	def test_08_listing_feed_and_offset_error(self):
+        self.assertEqual(response['data'], [])
 
-		self.request.errors = Mock(**{'add': Mock()})
+    def test_08_listing_feed_and_offset_error(self):
 
-		self.request.params['feed'] = 'changes'
-		self.request.params['offset'] = '0'
+        self.request.errors = Mock(**{'add': Mock()})
 
-		view = DummyResource(self.request, self.context)
+        self.request.params['feed'] = 'changes'
+        self.request.params['offset'] = '0'
 
-		class OffsetExpired(Exception):
-			""" Test exception for error_handler mocking"""
+        view = DummyResource(self.request, self.context)
 
-		with patch('openregistry.api.utils.error_handler',
-				   return_value=OffsetExpired):
-			with self.assertRaises(OffsetExpired):
-				response = view.get()
+        class OffsetExpired(Exception):
+            """ Test exception for error_handler mocking"""
+
+        with patch('openregistry.api.utils.error_handler',
+                   return_value=OffsetExpired):
+            with self.assertRaises(OffsetExpired):
+                response = view.get()
 
 
 def suite():
     tests = unittest.TestSuite()
-    tests.addTest(unittest.makeSuite(TestAPIResourceViews))
+    tests.addTest(unittest.makeSuite(TestAPIResourceListing))
     return tests
 
 
